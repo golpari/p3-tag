f0c2d6fad446867dc3403408dbc48e4e02b92494
golpari <golpari@umich.edu>
2023-11-27 06:51:34 -0700

    fix duplicate playerController

--- Assets/ExternalAssets/ElementalMeters/Scripts/ElementalPlayerController.cs
+++ Assets/ExternalAssets/ElementalMeters/Scripts/ElementalPlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class ElementalPlayerController : MonoBehaviour
 {
     private BoxCollider2D propBoxCollider;
     public MeterScript healthMeter; //meter code
