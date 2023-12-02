a5663b92895a9eee38008b541e8ddbd235771c64
Barbara Dreyer Rentz Ribeiro <bdreyer@umich.edu>
2023-12-02 12:50:19 -0600

    Fixed bounds on AllMovementHandeler

--- Assets/Scripts/Possession/TypesOfPossession/Movement/AllDirectionMovementHandler.cs
+++ Assets/Scripts/Possession/TypesOfPossession/Movement/AllDirectionMovementHandler.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 public class AllDirectionMovementHandler : PossessionActionBase, IMovable
 {
     private bool isActive = false;
-    [SerializeField] private float maxX = 10f;
-    [SerializeField] private float minX = -10f;
-    [SerializeField] private float maxZ = 10f;
-    [SerializeField] private float minZ = -10f;
-    [SerializeField] private float maxY = 10f;
-    [SerializeField] private float minY = -10f;
+    [SerializeField] private float maxX = 100f;
+    [SerializeField] private float minX = -100f;
+    [SerializeField] private float maxZ = 100f;
+    [SerializeField] private float minZ = -100f;
+    [SerializeField] private float maxY = 100f;
+    [SerializeField] private float minY = -100f;
 
     public override bool EnableAction()
     {
@@ -37,22 +37,23 @@ public class AllDirectionMovementHandler : PossessionActionBase, IMovable
         right.Normalize();
 
         // Read the input from the user
-        float moveX = currentMovementInput.x; // Left and right
-        float moveZ = currentMovementInput.y; // Up and down
-        float moveY = currentFloatInput.y;
+        float moveX = currentMovementInput.x * speed * Time.deltaTime; // Left and right
+        float moveZ = currentMovementInput.y * speed * Time.deltaTime; // Up and down
+        float moveY = currentFloatInput.y * speed * Time.deltaTime;
 
-        // constrain the new Y position
-        //moveY = Mathf.Clamp(moveY, minY, maxY);
+        Vector3 newPosition = transform.position + new Vector3(moveX, moveY, moveZ);
 
-        // Calculate the movement vector in world space
-        Vector3 movement = (forward * moveZ + right * moveX) * speed * Time.deltaTime;
-        movement.y = moveY * speed * Time.deltaTime;
+        //// Calculate the movement vector in world space
+        //Vector3 movement = (forward * moveZ + right * moveX) * speed * Time.deltaTime;
+        //movement.y = moveY * speed * Time.deltaTime;
 
-        /*// Constrain the new X and Z positions
-        movement.x = Mathf.Clamp(movement.x, minX, maxX);
-        movement.z = Mathf.Clamp(movement.z, minZ, maxZ);*/
+        // Constrain the new X and Z positions
+        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+        newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
+        newPosition.z = Mathf.Clamp(newPosition.z, minZ, maxZ);
 
-        transform.Translate(movement, Space.World);
+        //transform.Translate(movement, Space.World);
+        transform.position = newPosition;
 
     }
 }
